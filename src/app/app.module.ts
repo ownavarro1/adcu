c98ea7295ac208b4b4fdaa85d0771b8e27944edd
ownavarro1 <112905824+ownavarro1@users.noreply.github.com>
2023-06-05 12:27:57 -0500

    v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadersComponent } from './headers/headers.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadersComponent,
+    FooterComponent,
+    SearchComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
